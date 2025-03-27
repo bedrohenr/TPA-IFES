57fb9945d721297766b0642261825dff0fbb7e31
bedrohenr <ferfonhp@gmail.com>
2025-03-27 19:52:51 -0300

    main escrita corretamente. adicionando a string direta e não um node com string value

--- LinkedList/Principal.java
+++ LinkedList/Principal.java
@@ -3,17 +3,14 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Running");
 
-        LinkedList<Node<Integer>> ll = new LinkedList<>(); 
-        Node<Integer> n1 = new Node<>(10);
-        Node<Integer> n2 = new Node<>(15);
-        Node<Integer> n3 = new Node<>(9);
-        Node<Integer> n4 = new Node<>(10);
+        LinkedList<String> ll = new LinkedList<>(); 
 
-        ll.add(n1);
-        ll.add(n2);
-        ll.add(n3);
-        ll.add(n4);
+        ll.add("Oii");
+        ll.add("Helooo");
+        ll.add("Faaaaala Derick");
+        ll.add("Yo");
 
+        System.out.println("Tamanho da lista: " + ll.getSize());
         System.out.println(ll.toString());
     }
     
